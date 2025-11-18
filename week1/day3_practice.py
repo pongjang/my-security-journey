7ded16d8894de293d1494770100b6bcaed119b23
pongjang <seogyang558@gmail.com>
2025-11-18 12:42:34 +0900

    1주차 3일차: 파이썬 변수 및 리스트 학습 완료

---
+++ week1/day3_practice.py
@@ -0,0 +1,25 @@
+# 1. 변수(Variables) 실습
+# 님의 정보로 수정해 보세요.
+my_name = "홍길동" #문자열은 "" 또는''로 감쌉니다.
+my_age = 30 #숫자는 그냥 씁니다.
+my_goal = "AI 보안 융합 전문가"
+
+# print() 함수는 괄화안의 내용을 터미널에 출력합니다.
+print("---나의 정보---")
+print("이름:", my_name)
+print("나이:", my_age)
+print("목표:", my_goal)
+
+print("\n") # \n 은 줄바꿈 입니다
+
+#2. 리스트(List) 실습
+#좋아하는 메뉴 3개 채우기
+favorite_foods = ["치킨", "피자", "초밥"]
+
+print("---내가 좋아하는 음식---")
+print("첫번째:", favorite_foods[0]) # 파이썬은 0부터 숫자를 셉니다
+print("두번째:",favorite_foods[1])
+print("세번째:", favorite_foods[2])
+
+#리스트 전체를 출력할 수도 있습니다.
+print("전체목록:", favorite_foods)
