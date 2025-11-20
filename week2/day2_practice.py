ee33b8e4274f7a60436e6b1d0b1f85b615f60d66
pongjang <seogyang558@gmail.com>
2025-11-20 12:17:17 +0900

    2주차 2일차: 파일 입출력

---
+++ week2/day2_practice.py
@@ -0,0 +1,13 @@
+vulnerabilities = ["SQL injection", "XSS", "Weak Password"]
+
+with open("report.txt", "w", encoding="utf-8") as f:
+    f.write("security scan report:2025-11-20\n")
+    f.write("-" * 30 + "\n")
+
+    count =1
+    for bug in vulnerabilities:
+        f.write(f"{count}. {bug}\n")
+        count = count + 1
+
+    f.write("\n[complit]")
+print("report complite check report.txt")
